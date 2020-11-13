a0ccac75074aa5ddbf53cb26ea6d97a10770e60d
sow <sowousmane4811@gmail.com>
2020-11-13 11:16:21 +0100

    some modifications

--- main.c
+++ main.c
@@ -1,44 +1,82 @@
 #include<stdio.h>
 #include <time.h>
 #include<stdlib.h>
-#include "arraylist.h"
-#include "analyzer.h"
 #include "tas_bino.h"
+#include"analyzer.h"
+#include<math.h>
 
-
+/*11608883
+  11708934*/
+  
 int main(int argc, char ** argv){
-  int i,j;
-  struct tas_bino* tas_binomial = init_tas();
-  //allocation du noeud
-  struct tas_bino *tass=init_tas();
-  //struct node* tas2[10];
-//creation du premier tas
-for(i = 0; i < 10 ; i++)
+  int i;
+
+  struct node* tas_binomial = MAKE_bin_HEAP();
+  // Analyse du temps pris par les opérations.
+  analyzer_t * time_analysis = analyzer_create();
+  // Analyse du nombre de copies faites par les opérations.
+  analyzer_t * copy_analysis = analyzer_create();
+  // Analyse de l'espace mémoire inutilisé.
+  analyzer_t * memory_analysis = analyzer_create(); 
+  // Mesure de la durée d'une opération.
+  struct timespec before, after;
+  clockid_t clk_id = CLOCK_REALTIME;
+  // utilisé comme booléen pour savoir si une allocation a été effectuée.
+  //char memory_allocation; 
+	
+  //INSERTION CROISSANTE (Decommenter pour utiliser)
+	/*
+  for(i = 0; i <  1000 ; i++){
     // Ajout d'un élément et mesure du temps pris par l'opération.
-    tass= bin_HEAP_INSERT(tas_binomial, CREATE_NODE(i));
-   
-  
-  //creation du deuxieme tas
-   /*//tass->tas[0]= bin_HEAP_INSERT(tas_binomial, CREATE_NODE(7));
-    tass->tas[1]= bin_HEAP_INSERT(tas_binomial, CREATE_NODE(1));
-    tass->tas[2]= bin_HEAP_INSERT(tas_binomial, CREATE_NODE(3));
-    tass->tas[3]= bin_HEAP_INSERT(tas_binomial, CREATE_NODE(2));
-    tass->tas[4]= bin_HEAP_INSERT(tas_binomial, CREATE_NODE(15));
-   
-  
- //=======================================
-  */ // Affichage du tas
-    for(int i=0; i < 10 ; i++)
-      DISPLAY(tass->tas[i]);
-//=======================================
+    //clock_gettime(clk_id, &before);
+    bin_HEAP_INSERT(tas_binomial, CREATE_NODE(i));
+    //bin_HEAP_EXTRACT_MIN(tas_binomial);
+    //clock_gettime(clk_id, &after);
 
-  //=======================================================
-  //                 Union de deux tas
-  
+    // Enregistrement du temps pris par l'opération
+   // analyzer_append(time_analysis, after.tv_nsec - before.tv_nsec);
+    // Enregistrement du nombre de copies efféctuées par l'opération.
+    // S'il y a eu réallocation de mémoire, il a fallu recopier tout le tableau.
+    //analyzer_append(copy_analysis, (memory_allocation)? i:1 );
+    // Enregistrement de l'espace mémoire non-utilisé.
+   // analyzer_append(memory_analysis,arraylist_capacity(a)-arraylist_size(a));
+  }*/
 
-//==========================================================
-  //  Extract the min of the heap
-//===========================================================
+  //INSERTION Aleatoire(Decommenter pour utiliser)
+	
+    srand(time(NULL)); 
+    int nombre;
+    for(i = 0; i <  1000000 ; i++){
+      // Ajout d'un élément et mesure du temps pris par l'opération.
+      nombre = rand()%(1000000-0+1) + 0 ;
+      clock_gettime(clk_id, &before);
+      bin_HEAP_INSERT(tas_binomial, CREATE_NODE(nombre));
+      //memory_allocation = arraylist_pop_back(a);
+      clock_gettime(clk_id, &after);
+      // Enregistrement du temps pris par l'opération
+      analyzer_append(time_analysis, after.tv_nsec - before.tv_nsec);
+      // Enregistrement du nombre de copies efféctuées par l'opération.
+      // S'il y a eu réallocation de mémoire, il a fallu recopier tout le tableau.
+      //analyzer_append(copy_analysis, (memory_allocation)? i:1 );
+      // Enregistrement de l'espace mémoire non-utilisé.
+      // analyzer_append(memory_analysis,arraylist_capacity(a)-arraylist_size(a));
+  }
 
+  /*// Affichage de quelques statistiques sur l'expérience.
+  fprintf(stderr, "Total cost: %Lf\n", get_total_cost(time_analysis));
+  fprintf(stderr, "Average cost: %Lf\n", get_average_cost(time_analysis));
+  fprintf(stderr, "Variance: %Lf\n", get_variance(time_analysis));
+  fprintf(stderr, "Standard deviation: %Lf\n", get_standard_deviation(time_analysis));
+
+  // Sauvegarde les données de l'expérience.
+  save_values(time_analysis, "./plots/dynamic_array_time_c.plot");
+  //save_values(copy_analysis, "../plots/dynamic_array_copy_c.plot");
+  //save_values(memory_analysis, "../plots/dynamic_array_memory_c.plot");
+
+  // Nettoyage de la mémoire avant la sortie du programme
+ // heap_free(tas_binomial);
+  analyzer_destroy(time_analysis);
+  analyzer_destroy(copy_analysis);
+  analyzer_destroy(memory_analysis);*/
   return EXIT_SUCCESS;
 }
